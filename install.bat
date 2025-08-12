5cd54c4d9e8fd86b25212ba1c83ca6397d51b4ad
Admin <gs.techdeveloper@gmail.com>
2025-08-12 18:59:28 -0300

    Alteração final do install.bat

--- install.bat
+++ install.bat
@@ -1,3 +1,18 @@
-python -m venv ./
-Scripts/activate.bat
+ECHO Criando o ambiente virtual...
+
+python -m venv .
+
+cls
+
+ECHO Ativando ambiente virtual...
+call Scripts\activate.bat
+
+cls
+
+ECHO Instalando os pacotes...
 pip install -r requirements.txt
+
+cls
+
+ECHO Instalando o PlayWright...
+playwright install
